e57faa81df30d3425633d69789f9925232052433
Douglas Santiago <douglassantiagods@hotmail.com>
2022-03-20 09:38:55 -0300

    alteração js para ts

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { registerRootComponent } from 'expo';
 
-import App from './src/routers/login/login';
+import App from './src/router/SignIn/index.tsx';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
---
+++ src/routers/SignUp/index.tsx
@@ -0,0 +1,15 @@
+export default function SignIn() {
+  return (
+    <Container>
+
+      <Title>teste</Title>
+      <InputText />
+      <InputText />
+      <Form>
+      <Input><Text>Login</Text></Input>
+      <Input><Text>Cadastrar</Text></Input>
+      </Form>
+      <Link/>
+    </Container>
+  )
+}
